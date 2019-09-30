667beddb294cfba4f55833061eb6f8cd11b8129c
Rilas00 <55482372+Rilas00@users.noreply.github.com>
2019-09-30 09:54:00 +0200

    Add files via upload

---
+++ Operacions.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne
+ */
+public class Operacions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+ Scanner entrada = new Scanner(System.in);
+ System.out.println("Introdueix un nombre a:"); 
+ int a = entrada.nextInt();
+ System.out.println("Introdueix el nombre b:"); 
+ int b = entrada.nextInt(); 
+ 
+ if (a<=b) {
+} else { 
+   System.out.println("Error, el nombre b es més gran que el nombre a");
+
+  if (a>=b) {
+      
+  }
+  else { 
+      } 
+   System.out.println("Elegeix l'operació que vols realitzar: si vols sumar escriu s, si vols restar r, si vols multiplicar m i si vols dividir d.")
+   int suma; 
+   int r;
+   int m;
+   int d;
+   int resultat;
+   int operacio = entrada.nextInt();        
+   if (operacio=suma) { 
+    }
+   else {  int resultatsuma = resultat=a+b; 
+       
+   }
